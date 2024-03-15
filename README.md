a4b04500c06de884878b5a501aa08b7759bfb64f
Julihermes Melo <julihermes@hotmail.com>
2024-03-15 16:46:41 -0300

    Initial commit. First Version.

---
+++ README.md
@@ -0,0 +1,11 @@
+# MY PERSONAL WINDOWS APPS
+
+## How to
+- Open Powershell as admin.
+- run this command to temporarily unblock to exec scripts: `Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process`.
+- Wait to respond any prompt until the script ends.
+
+## To do after
+- Install SauceCodePro font here: https://www.nerdfonts.com/font-downloads
+- Install Terminal Theme here: https://github.com/catppuccin/windows-terminal
+- Set theme, font and default console in Windows Terminal settings.
---
+++ install.ps1
@@ -0,0 +1,51 @@
+Clear-Host
+Write-Output "MY PERSONAL WINDOWS APPS"
+
+$apps = @(
+    @{name = "Microsoft.PowerShell" },
+    @{name = "Starship.Starship" },
+    @{name = "junegunn.fzf" },
+    @{name = "ajeetdsouza.zoxide" },
+    @{name = "lsd-rs.lsd" },
+    @{name = "Microsoft.PowerToys" }
+    @{name = "RARLab.WinRAR" },
+    @{name = "9NJ3KMH29VGJ" }, #Enpass
+    @{name = "Google.Chrome" },
+    @{name = "Arc.Arc" },
+    @{name = "Readdle.Spark" },
+    @{name = "9NKSQGP7F2NH" }, # Whatsapp
+    @{name = "Telegram.TelegramDesktop" },
+    @{name = "Ytmdesktop.Ytmdesktop" },
+    @{name = "SumatraPDF.SumatraPDF" },
+    @{name = "Logitech.GHUB" },
+    @{name = "ShareX.ShareX" },
+    @{name = "9PM860492SZD" } # PC Manager
+);
+
+$CONTINUE = Read-Host -Prompt 'Would you like to continue with the install? (y,n)'
+if ($CONTINUE -eq 'y' || $CONTINUE -eq 'Y') {
+  Write-Output "Starting..."
+} else {
+  Write-Output "This script will now exit, no changes were made to your system."
+  exit
+}
+
+Write-Output "Installing apps, this may take a while..."
+Foreach ($app in $apps) {
+    $listApp = winget list --exact -q $app.name
+    if (![String]::Join("", $listApp).Contains($app.name)) {
+        Write-host "Installing: " $app.name
+        winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name
+    }
+    else {
+        Write-host "Skipping: " $app.name " (already installed)"
+    }
+}
+
+Write-Output "Coping starship file..."
+cp config\starship.toml $HOME\.config\starship.toml
+
+Write-Output "Coping profile file..."
+cp config\profile.ps1 $PROFILE.CurrentUserAllHosts
+
+Write-Output "Script had completed!"
